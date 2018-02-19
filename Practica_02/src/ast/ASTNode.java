94341145070ab096829d78a5a344b496fc5f6971
Pablo Menéndez Suárez <pabloyo97@hotmail.com>
2018-02-19 13:37:40 +0100

    fixed ast

--- Practica_02/src/ast/ASTNode.java
+++ Practica_02/src/ast/ASTNode.java
@@ -4,4 +4,7 @@ public interface ASTNode {
 
 	int DEFAULT_ROW_COLUMN = 0;
 
+	public int getLine();
+
+	public int getColumn();
 }
--- Practica_02/src/ast/Statement.java
+++ Practica_02/src/ast/Statement.java
@@ -1,11 +1,5 @@
 package ast;
 
-public interface Statement {
-
-	int getColumn();
-
-	int getLine();
-
-	String toString();
+public interface Statement extends ASTNode {
 
 }
