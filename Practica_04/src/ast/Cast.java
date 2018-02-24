8d6363a5d1b560d8192f43e956ca12d181ad1c8f
mistermboy <pabloyo97@hotmail.com>
2018-02-24 13:42:04 +0100

    more IExpression

---
+++ Practica_04/src/ast/Cast.java
@@ -0,0 +1,62 @@
+package ast;
+
+public class Cast implements Expression {
+
+	private int row = ASTNode.DEFAULT_ROW_COLUMN;
+	private int column = ASTNode.DEFAULT_ROW_COLUMN;
+
+	private Expression exp;
+	private Cast castType;
+
+	public Cast(int row, int column, Expression exp, Cast castType) {
+		super();
+		this.row = row;
+		this.column = column;
+		this.exp = exp;
+		this.castType = castType;
+	}
+
+	@Override
+	public int getLine() {
+		return row;
+	}
+
+	@Override
+	public int getColumn() {
+		return column;
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+	public Expression getExp() {
+		return exp;
+	}
+
+	public void setExp(Expression exp) {
+		this.exp = exp;
+	}
+
+	public Cast getCastType() {
+		return castType;
+	}
+
+	public void setCastType(Cast castType) {
+		this.castType = castType;
+	}
+
+	public void setColumn(int column) {
+		this.column = column;
+	}
+
+	@Override
+	public String toString() {
+		return "Cast [row=" + row + ", column=" + column + ", exp=" + exp + ", castType=" + castType + "]";
+	}
+
+}
